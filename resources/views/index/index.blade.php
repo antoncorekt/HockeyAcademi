f9b3cd500851840d000716e87465c1f44fd60282
NadineAstakhova <nadine.astakhova@gmail.com>
2018-01-22 14:47:03 +0200

    fix mobile background

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -8,6 +8,7 @@
 <script type="text/javascript" src="{{asset('js/Snow.js')}}"></script>
 <script type="text/javascript" src="{{asset('js/SnowManaged.js')}}"></script>
 <script type="text/javascript" src="{{asset('js/VideoManager.js')}}"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 @endsection
 
 @section('before-nav')
