ae94e847d04d78c424aebfece70b9bef9608aff7
Козловський Антон Олександрович <kozlovskyi.a@donnu.edu.ua>
2018-01-24 13:36:28 -0800

    script changes

--- public/js/VideoManager.js
+++ public/js/VideoManager.js
@@ -3,35 +3,31 @@ $(document).ready(function () {
     var el = document.getElementById("video");
     var flag = true;
 
-    $(".cont").css("display", "block");
+    function start_snow(){
+        if (flag){
+            init();
+            flag = false;
+        }
+    }
 
-    init();
-
-    // function start_snow(){
-    //     if (flag){
-    //         init();
-    //         flag = false;
-    //     }
-    // }
-
-    // if (el == null || el == undefined){
-    //     console.log("Element video not found")
-    //     $(".cont").css("display", "block");
-    //     start_snow();
-    // }
+    if (el == null || el == undefined){
+        console.log("Element video not found")
+        $(".cont").css("display", "block");
+        start_snow();
+    }
 
 
 
-    // $("#video-tag").on('ended', function () {
-    //     $(".cont").css("display", "block");
-    //     $("#video").remove();
-    //     start_snow();
-    // });
+    $("#video-tag").on('ended', function () {
+        $(".cont").css("display", "block");
+        $("#video").remove();
+        start_snow();
+    });
 
 
-    // $(".video-skip").click(function(){
-    //     $(".cont").css("display", "block");
-    //     $("#video").remove();
-    //     start_snow();
-    // })
+    $(".video-skip").click(function(){
+        $(".cont").css("display", "block");
+        $("#video").remove();
+        start_snow();
+    })
 });
