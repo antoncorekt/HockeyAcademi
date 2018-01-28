3ff019f8b8473eee49330c0f192b065e37e20853
NadineAstakhova <nadine.astakhova@gmail.com>
2018-01-29 00:32:14 +0200

    add logo to mobile design

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -36,6 +36,7 @@
 <div class="container-fluid" id="body-main">
     <nav class="navbar navbar-default" role="navigation">
         <div class="navbar-header">
+            <img src="../images/logo-test.png" id="logo-navheader">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse" id="navbar-btn">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
